014a9242b8912f02fa3be9197c52759f444f75cf
WenDeHor <mazur23252@gmail.com>
2022-12-27 23:48:27 +0200

    3.Second Page

---
+++ app/src/main/java/com/example/flipmeet/ActivitySearch.java
@@ -0,0 +1,13 @@
+package com.example.flipmeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
--- app/src/main/java/com/example/flipmeet/MainActivity.java
+++ app/src/main/java/com/example/flipmeet/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.flipmeet;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button act_change;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        act_change = (Button) findViewById(R.id.activity_search);
+        act_change.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent toMap = new Intent(".ActivitySearch");
+                        startActivity(toMap);
+                    }
+                }
+        );
     }
 }
