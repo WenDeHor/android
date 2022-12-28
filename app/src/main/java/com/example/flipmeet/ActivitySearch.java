1b2468596cda7e3689a287a2773095c5232a659a
WenDeHor <mazur23252@gmail.com>
2022-12-28 23:04:27 +0200

    6.First DB Local

--- app/src/main/java/com/example/flipmeet/ActivitySearch.java
+++ app/src/main/java/com/example/flipmeet/ActivitySearch.java
@@ -1,5 +1,9 @@
 package com.example.flipmeet;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,22 @@ public class ActivitySearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    public void onClick(View view){
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER, UNIQUE(name))");
+        db.execSQL("INSERT OR IGNORE INTO users VALUES ('Tom Smith', 23), ('John Dow', 31);");
+
+        Cursor query = db.rawQuery("SELECT * FROM users;", null);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("");
+        while(query.moveToNext()){
+            String name = query.getString(0);
+            int age = query.getInt(1);
+            textView.append("Name: " + name + " Age: " + age + "\n");
+        }
+        query.close();
+        db.close();
     }
 }
